09ca2ce23ba6bda7b76bc1b54b961d068cdeeef7
LahsivK4070 <vishalkumarcoold2@gmail.com>
2022-08-18 20:10:01 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="hit-area">
+        <div className="hitbox" id="a-1"></div>
+        <div className="hitbox" id="a-2"></div>
+        <div className="hitbox" id="a-3"></div>
+        <div className="hitbox" id="a-4"></div>
+        <div className="hitbox" id="a-5"></div>
+        <div className="hitbox" id="a-6"></div>
+        <div className="hitbox" id="a-7"></div>
+        <div className="hitbox" id="a-8"></div>
+        <div className="hitbox" id="b-1"></div>
+        <div className="hitbox" id="b-2"></div>
+        <div className="hitbox" id="b-3"></div>
+        <div className="hitbox" id="b-4"></div>
+        <div className="hitbox" id="b-5"></div>
+        <div className="hitbox" id="b-6"></div>
+        <div className="hitbox" id="b-7"></div>
+        <div className="hitbox" id="b-8"></div>
+        <div className="hitbox" id="c-1"></div>
+        <div className="hitbox" id="c-2"></div>
+        <div className="hitbox" id="c-3"></div>
+        <div className="hitbox" id="c-4"></div>
+        <div className="hitbox" id="c-5"></div>
+        <div className="hitbox" id="c-6"></div>
+        <div className="hitbox" id="c-7"></div>
+        <div className="hitbox" id="c-8"></div>
+        <div className="hitbox" id="d-1"></div>
+        <div className="hitbox" id="d-2"></div>
+        <div className="hitbox" id="d-3"></div>
+        <div className="hitbox" id="d-4"></div>
+        <div className="hitbox" id="d-5"></div>
+        <div className="hitbox" id="d-6"></div>
+        <div className="hitbox" id="d-7"></div>
+        <div className="hitbox" id="d-8"></div>
+        <div className="hitbox" id="e-1"></div>
+        <div className="hitbox" id="e-2"></div>
+        <div className="hitbox" id="e-3"></div>
+        <div className="hitbox" id="e-4"></div>
+        <div className="hitbox" id="e-5"></div>
+        <div className="hitbox" id="e-6"></div>
+        <div className="hitbox" id="e-7"></div>
+        <div className="hitbox" id="e-8"></div>
+        <div className="eye">
+          <div className="pupil"></div>
+        </div>
+        <div className="eye">
+          <div className="pupil"></div>
+        </div>
+        <div className="eyelid"></div>
+        <div className="eyelid"></div>
+        <div className="circle"></div>
+        <div className="lips"></div>
+        <div className="hat">
+          <div className="hat-top"></div>
+          <div className="hat-bottom"></div>
+        </div>
+      </div>
     </div>
   );
 }
